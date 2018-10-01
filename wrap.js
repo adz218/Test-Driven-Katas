2b46eb7c41912e877acc78d7967db84cd79213a9
Adam Zhou <adzhou218@gmail.com>
2018-10-01 11:51:16 -0400

    second commit

--- wrap.js
+++ wrap.js
@@ -12,3 +12,44 @@ sit eu amet, elit na
 magna sem amet nulla
 vel purus ac ligula.
 */
+
+//attempt at wrap
+// const wrap = (line, maxLen) => {
+//   let cutIdx = 0;
+//   let sliceEndpoint = maxLen;
+//   let resultStr = "";
+//
+//   while (cutIdx < line.length) {
+//     resultStr += line.slice(cutIdx, sliceEndpoint) + "\n";
+//     cutIdx += maxLen;
+//     sliceEndpoint += maxLen;
+//   }
+//
+//   return resultStr.slice(0, resultStr.length - 1);
+// };
+
+function wrap(line, maxLen) {
+  if (line.length <= maxLen) {
+    return line;
+  }
+
+  const indexOfBlank = line.lastIndexOf(" ", maxLen);
+  let split;
+  let offset;
+
+  if (indexOfBlank > -1) {
+    split = indexOfBlank;
+    offset = 1;
+  } else {
+    split = maxLen;
+    offset = 0;
+  }
+
+  return (
+    line.substring(0, split) +
+    "\n" +
+    wrap(line.substring(split + offset), maxLen)
+  );
+}
+
+module.exports = wrap;
--- wrap.spec.js
+++ wrap.spec.js
@@ -6,4 +6,29 @@ describe("wrap", () => {
   it("Returns empty string if empty string was provided", () => {
     expect(wrap("", 10)).to.equal("");
   });
+
+  it("Should return a single line if string provided is less than maxLen argument", () => {
+    expect(wrap("Our names are Adam+Christian", 100)).to.equal(
+      "Our names are Adam+Christian"
+    );
+  });
+
+  it("Should return multiple lines if string provided is greater than maxLen argument", () => {
+    expect(wrap("Our names are Adam+Christian", 10)).to.equal(
+      `Our names \nare Adam+C\nhristian`
+    );
+    //if multiple assertions/expects are written within one it block,
+    //subsequent assertions will only hit after the prior assertion passes
+    expect(wrap("Our names are Adam+Christian", 14)).to.equal(
+      "Our names are \nAdam+Christian"
+    );
+    expect(
+      wrap(
+        "Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.",
+        20
+      )
+    ).to.equal(
+      "Lorem ipsum dolor   \nsit eu amet, elit na\nmagna sem amet nulla\nvel purus ac ligula."
+    );
+  });
 });
