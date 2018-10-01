bf9f868bde671c06967a77d05bd6fa06383eadf0
Christian Mejia <chrismejia10@gmail.com>
2018-10-01 11:07:11 -0400

    Switch because mocha not working here

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pair Exercise: Test-Driven Katas
+
+The only time we'll want to fail before we pass, repeatedly.
---
+++ wrap.js
@@ -0,0 +1,14 @@
+/*
+Wrap should format a string, such that there are no characters passed a given number designated character amount per column.
+
+For example:
+
+wrap("Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.", 20)
+
+Should return
+
+Lorem ipsum dolor
+sit eu amet, elit na
+magna sem amet nulla
+vel purus ac ligula.
+*/
